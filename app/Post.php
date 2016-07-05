766cb10db25b8c5f5eef49e9b63c1cb064dc678e
xinxin <xinxin@rrkd.cn>
2016-07-05 19:09:56 +0800

    add post

--- app/Post.php
+++ app/Post.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     //
+    protected $dates = ['published_at'];
+
+    public function setTitleAttribute($value){
+        $this->attribute['title'] = $value;
+
+        if(!$this->exists){
+            $this->attribute['slug'] = str_slug($value);
+        }
+    }
 }
