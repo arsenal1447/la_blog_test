c55033af2fe5468656879e52e076997ade6023e6
xinxin <xinxin@rrkd.cn>
2016-07-07 17:37:14 +0800

    修复无法登陆的问题

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -32,9 +32,9 @@ Route::group(['namespace'=>'Admin','middleware'=>'auth'],function(){
 
 
 //logging in and out
-Route::get('/auth/login','Auth\AuthController@getLogin');
-Route::get('/auth/login','Auth\AuthController@postLogin');
-Route::get('/auth/logout','Auth\AuthController@getLogout');
+Route::get('/login','Auth\AuthController@getLogin');
+Route::post('/login','Auth\AuthController@postLogin');
+Route::get('/logout','Auth\AuthController@getLogout');
 
 
 
