85f94322f156b72debc19bfd248ff7b6fc6a7b6b
xinxin <xinxin@rrkd.cn>
2016-07-07 11:26:16 +0800

    add new route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,3 +17,50 @@ Route::get('/', function () {
 
 Route::get('blog', 'BlogController@index');
 Route::get('blog/{slug}', 'BlogController@showPost');
+
+
+//admin array_intersect_uassoc
+Route::get('admin',function(){
+    return redirect('/admin/post');
+});
+
+Route::group(['namespace'=>'Admin','middleware'=>'auth'],function(){
+    Route::resource('admin/post','PostController');
+    Route::resource('admin/tag','TagController');
+    Route::get('admin/upload','UploadController@index');
+});
+
+
+//logging in and out
+Route::get('/auth/login','Auth\AuthController@getLogin');
+Route::get('/auth/login','Auth\AuthController@postLogin');
+Route::get('/auth/logout','Auth\AuthController@getLogout');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
