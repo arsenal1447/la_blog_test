f833ce0d727a8f7b6385ee18d4a27ee502269c2f
xinxin <xinxin@rrkd.cn>
2016-07-07 17:22:38 +0800

    edit some files

--- resources/views/admin/blog/index.blade.php
+++ resources/views/admin/blog/index.blade.php
@@ -1,11 +1,11 @@
 @extends('admin.layout')
 
-@<section>
+@section('content')
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel panel-heading">
+                    <div class="panel-heading">
                         <h3 class="panel-title">Posts</h3>
                     </div>
                     <div class="panel-body">
