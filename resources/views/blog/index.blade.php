ce61b8a1fd60c20f4b442898159145c2fa440c6a
xinxin <xinxin@rrkd.cn>
2016-07-07 10:44:35 +0800

    完成初步blog

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -10,7 +10,7 @@
             <h5>Page {{ $posts->currentPage() }} of {{ $posts->lastPage() }}</h5>
             <hr>
             <ul>
-            @foreach ($variable as $key => $value): ?>
+            @foreach ($posts as $post)
                 <li>
                     <a href="/blog/{{ $post->slug }}">{{ $post->title }}</a>
                     <em>({{ $post->published_at }})</em>
