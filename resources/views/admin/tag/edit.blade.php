5138bca281e79642fca69e93abb739e31c91342f
xinxin <xinxin@rrkd.cn>
2016-08-01 15:31:25 +0800

    edit some code

--- resources/views/admin/tag/edit.blade.php
+++ resources/views/admin/tag/edit.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container-fluid">
-    <div class="row page-title-row">        
+    <div class="row page-title-row">
         <div class="col-md-12">
             <h3>Tags <small>» Edit Tag</small></h3>
         </div>
@@ -15,14 +15,15 @@
                     <h3 class="panel-title">Tag Edit Form</h3>
                 </div>
                 <div class="panel-body">
-                    @include('admin.partials'.errors)
-                    @include('admin.partials'.success)
 
-                    <form class="form-horizontal" role="form" method="POST" action="/admin/tag{{ $id }}">
+                    @include('admin.partials.errors')
+                    @include('admin.partials.success')
+
+                    <form class="form-horizontal" role="form" method="POST" action="/admin/tag/{{ $id }}">
                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
                         <input type="hidden" name="_method" value="PUT">
                         <input type="hidden" name="id" value="{{ $id }}">
-                        
+
                         <div class="form-group">
                             <label for="tag" class="col-md-3 control-label">Tag</label>
                             <div class="col-md-3">
@@ -30,23 +31,24 @@
                             </div>
                         </div>
 
-                        @include('admin.tag_form')
-                        
+                        @include('admin.tag._form')
+
                         <div class="form-group">
                             <div class="col-md-7 col-md-offset-3">
                                 <button type="submit" class="btn btn-primary btn-md">
                                     <i class="fa fa-save"></i>
-                                    Save Change
+                                    Save Changes
                                 </button>
                                 <button type="button" class="btn btn-danger btn-md" data-toggle="modal" data-target="#modal-delete">
                                     <i class="fa fa-times-circle"></i>
                                     Delete
                                 </button>
-                                <p class="form-control-static">{{ $tag }}</p>
+
                             </div>
                         </div>
 
                     </form>
+
                 </div>
             </div>
         </div>
@@ -54,12 +56,12 @@
 </div>
 
 {{-- 确认删除 --}}
-<div class="modal fade" id="modal-delete" tabindex="-1">
+<div class="modal fade" id="modal-delete" tabIndex="-1">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal">
-                    x
+                    ×
                 </button>
                 <h4 class="modal-title">Please Confirm</h4>
             </div>
@@ -74,8 +76,8 @@
                     <input type="hidden" name="_token" value="{{ csrf_token() }}">
                     <input type="hidden" name="_method" value="DELETE">
                     <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-danger" >
-                        <i class="fa fa-times-circle"></i>Yes
+                    <button type="submit" class="btn btn-danger">
+                        <i class="fa fa-times-circle"></i> Yes
                     </button>
                 </form>
             </div>
