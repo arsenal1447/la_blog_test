6634133add79cf9ccb7df72df71a0bbc185f522d
xinxin <xinxin@rrkd.cn>
2016-08-01 18:11:38 +0800

    添加上传相关文件

--- app/Http/Controllers/Admin/UploadController.php
+++ app/Http/Controllers/Admin/UploadController.php
@@ -6,8 +6,23 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use App\Services\UploadManager;
 
 class UploadController extends Controller
 {
-    //
+    protected $manager;
+
+    public function __construct(UploadManager $manager){
+        $this->manager = $manager;
+    }
+    
+    /**
+     * Show page of files / subfolders
+     */
+     public function index(Request $request){
+         $folder = $request->get('folder');
+         $data = $this->manager->folderInfo($folder);
+
+         return view('admin.upload.index',$data);
+     }
 }
