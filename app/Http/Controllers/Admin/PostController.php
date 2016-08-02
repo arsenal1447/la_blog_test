606173f018447cdc0a1422c9c037c16e35a1c822
xinxin <xinxin@rrkd.cn>
2016-08-02 14:23:38 +0800

    1，添加辅助函数 2，添加增删改查方法

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -1,10 +1,12 @@
 <?php
 namespace App\Http\Controllers\Admin;
 
-// use Illuminate\Http\Request;
-//
-// use App\Http\Requests;
+use App\Jobs\PostFormFields;
+use App\Http\Requests;
+use App\Http\Requests\PostCreateRequest;
+use App\Http\Requests\PostUpdateRequest;
 use App\Http\Controllers\Controller;
+use App\Post;
 
 class PostController extends Controller
 {
@@ -14,6 +16,85 @@ class PostController extends Controller
      * @return Response
      */
     public function index(){
-        return view('admin.post.index');
+        return view('admin.post.index')
+                ->withPosts(Post::all());
     }
+
+    /**
+     * show the new post form
+     */
+    public function create()
+    {
+        $data = $this->dispatch(new PostFormFields());
+
+        return view('admin.post.create',$data);
+    }
+
+    /**
+     * store a newly created post
+     * @param PostCreateRequest $request
+     */
+    public function store(PostCreateRequest $request)
+    {
+        $post = Post::create($request->postFillData());
+        $post->syncTags($request->get('tags'),[]);
+
+        return redirect()
+                ->route('admin.post.index')
+                ->withSuccess('New Post Successfully Created.')
+    }
+
+    /**
+     * Show the post edit form
+     *
+     * @param int $id
+     * @return Response
+     */
+    public function edit($id)
+    {
+        $data = $this->dispatch(new PostFormFields($id));
+
+        return view('admin.post.edit',$data);
+    }
+
+
+    /**
+     * Update the post
+     *
+     * @param PostCreateRequest $request
+     * @param int $id
+     */
+    public function update(PostCreateRequest $request, $id)
+    {
+        $post = Post::findOrFail($id);
+        $post->fill($request->postFillData());
+        $post->save();
+        $post->syncTags($request->get('tags'),[]));
+
+        if($request->action==='continue')
+        {
+            return redirect()
+                            ->back()
+                            ->withSuccess('Post saved.');
+        }
+        return redirect()
+                        ->route()
+                        ->withSuccess('Post saved.');
+    }
+
+
+    public function destroy($id)
+    {
+        $post = Post::findOrFail($id);
+        $post->tags()->detach();
+        $post->delete();
+
+        return redirect()
+                        ->route('admin.post.index')
+                        ->withSuccess('Post deleted.');
+
+    }
+
+
+
 }
