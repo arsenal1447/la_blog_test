b9e00c3bcac03be908128dd8ee82704eb627b605
xinxin <xinxin@rrkd.cn>
2016-08-05 13:33:25 +0800

    add site map

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -8,6 +8,7 @@ use App\Post;
 use App\Tag;
 use Illuminate\Http\Request;
 use App\Services\RssFeed;
+use App\Services\SiteMap;
 
 class BlogController extends Controller
 {
@@ -38,5 +39,13 @@ class BlogController extends Controller
 
         return response($rss)
           ->header('Content-type', 'application/rss+xml');
+    }
+
+    public function siteMap(SiteMap $siteMap)
+    {
+        $map = $siteMap->getSiteMap();
+
+        return response($map)
+            ->header('Content-type','text/xml');
     }
 }
---
+++ app/Services/SiteMap.php
@@ -0,0 +1,72 @@
+<?php
+namespace App\Services;
+
+use App\Post;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Cache;
+
+
+class SiteMap
+{
+    /**
+     * Return the content of the Site Map
+     */
+    public function getSiteMap()
+    {
+        if(Cache::has('site_map')){
+            return Cache::get('site_map');
+        }
+
+        $sitemap = $this->buildSiteMap();
+        Cache::add('site-map',$sitemap, 120);
+        return $sitemap;
+    }
+
+    /**
+     * Build the Site Map
+     */
+    protected function buildSiteMap()
+    {
+        $postinfo = $this->getPostsInfo();
+        $dates = array_values($postinfo);
+        sort($dates);
+        $lastmod = last($dates);
+        // $url = trim(url(),'/').'/';
+        $url = str_finish(url('/'), '/');
+
+        $xml = [];
+        $xml[] = '<?xml version="1.0" encodeing="UTF-8"?'.'>';
+        $xml[] = '<urlset xmlns = "http://www.sitemaps.org/schemas/sitemap/0.9">';
+        $xml[] = '  <url>';
+        $xml[] = "      <loc>$url</loc>";
+        $xml[] = "      <lastmod>$lastmod</lastmod>";
+        $xml[] = '      <changefreq>daily</changefreq>';
+        $xml[] = '      <priority>0.8</priority>';
+        $xml[] = '  </url>';
+
+        foreach($postinfo as $slug => $lastmod){
+            $xml[] = '  <url>';
+            $xml[] = "      <loc>{$url}blog/$slug</loc>";
+            $xml[] = "      <lastmod>$lastmod</lastmod>";
+            $xml[] = '  </url>';
+        }
+
+        $xml[] = '</urlset>';
+
+        return join("\n",$xml);
+    }
+
+    /**
+   * Return all the posts as $url => $date
+   */
+    protected function getPostsInfo()
+    {
+        return Post::where('published_at','<=', Carbon::now())
+            ->where('is_draft',0)
+            ->orderBy('published_at','desc')
+            ->lists('updated_at','slug')
+            ->all();
+    }
+}
+
+ ?>
