e7f5422f7b0df3f49661807cb80a5a80d9208493
xinxin <xinxin@rrkd.cn>
2016-08-22 10:53:28 +0800

    修改 图片上传显示错误

--- config/blog.php
+++ config/blog.php
@@ -3,14 +3,14 @@ return [
         'name'=>'My blog',
         'title'=>'My blog',
         'subtitle'=>'http://local.blog_test.com',
-        'description'=>'Laravel学院致力于提供优质Laravel中文学习资源',
+        'description'=>'Laravel学自学blog',
         'author'=>'zxx',
         'page_image'=>'home-bg.jpg',
         'posts_per_page'=>10,
         'rss_size' => 25,
         'uploads'=>[
                 'storage'=>'local',
-                'webpath'=>'/uploads/laravel wenjianjia',
+                'webpath'=>'/uploads',
         ],
         'contact_email'=>env('MAIL_FROM'),
 ];
